2ab81289672808616e6cd165672273941aefbaad
Debbie <asamoahjaeldeborah@gmail.com>
2022-10-29 16:48:03 +0000

    Made changes to the slider data

---
+++ src/data/sliderData.js
@@ -0,0 +1,33 @@
+import One from "../assets/images/CH2 1.png"
+import Two from "../assets/images/CO1 2.png"
+import Three from "../assets/images/DS1 2.png"
+
+
+// import One from "../assets/logo.svg"
+// import Two from "../assets/images/CH3.png"
+// import Three from "../assets/images/CH4.png"
+
+
+const SliderData  = [
+    {
+        image: One,
+        subtitle: "CHAIR COLLECTION 2022",
+        title: "WELCOME TO HOMEO",
+        desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+        image: Two,
+        subtitle: "CHAIR COLLECTION 2022",
+        title: "WELCOME TO HOMEO",
+        desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+        image: Three,
+        subtitle: "CHAIR COLLECTION 2022",
+        title: "WELCOME TO HOMEO",
+        desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+];
+
+export default SliderData;
+
