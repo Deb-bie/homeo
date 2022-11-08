093c1b48ea42344c770aefbc3ae5eb916259ebe7
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-08 17:55:05 +0000

    Created dummy data for the various components

---
+++ src/data/blogData.js
@@ -0,0 +1,25 @@
+
+import Image from "../assets/images/1.jpg"
+
+const BlogData = [
+    {
+        id: 1,
+        image: Image,
+        title: "A new way to beautify your home in 2022",
+        date: "November 18, 2022"
+    },
+    {
+        id: 2,
+        image: Image,
+        title: "A new way to beautify your home in 2022",
+        date: "November 18, 2022"
+    },
+    {
+        id: 3,
+        image: Image,
+        title: "A new way to beautify your home in 2022",
+        date: "November 18, 2022"
+    }
+]
+
+export default BlogData;
---
+++ src/data/categoriesData.js
@@ -0,0 +1,46 @@
+import Blob from "../assets/blobs/BL1.svg"
+import Chair from "../assets/images/CH2 1.png"
+import Couch from "../assets/images/CO1 2.png"
+import Bed from "../assets/images/CH2 1.png"
+import Table from "../assets/images/CH2 1.png"
+import Dining from "../assets/images/DS1 2.png"
+import Light from "../assets/images/CH2 1.png"
+
+
+
+
+
+const CategoryData = [
+    {
+        category: "chairs",
+        blob: Blob,
+        image: Chair,
+    },
+    {
+        category: "couches",
+        blob: Blob,
+        image: Couch,
+    },
+    {
+        category: "beds",
+        blob: Blob,
+        image: Bed,
+    },
+    {
+        category: "tables",
+        blob: Blob,
+        image: Table,
+    },
+    {
+        category: "Dining Sets",
+        blob: Blob,
+        image: Dining,
+    },
+    {
+        category: "lights",
+        blob: Blob,
+        image: Light,
+    }
+]
+
+export default CategoryData;
