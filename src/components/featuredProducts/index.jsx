45179a1e2c1a6395677694d36f9db6543c605e58
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-07 12:22:14 +0000

    Made changes to the featured products section

--- src/components/featuredProducts/index.jsx
+++ src/components/featuredProducts/index.jsx
@@ -1,80 +1,69 @@
+import { useNavigate} from "react-router-dom"
 import { BsArrowRight } from "react-icons/bs";
 
-import Image1 from "../../assets/images/CH2 1.png"
+import FeaturedProduct from "../../data/featuredProduct.js"
+
 
 const FeaturedProducts = () => {
+    const navigate = useNavigate()
+
+    const handleDescription = (id) => {
+        navigate(`/products/${id}`)
+    }
+
+
     return (
         <div className="w-[100%] h-full mt-24 flex overflow-hidden ">
             <div className="w-[100%] h-full flex flex-col flex-nowrap content-center" >
+
+                {
+                    FeaturedProduct.map((featuredProduct, id) => (
+                        <div key={id}>
+                            {
+                                id === 1 ? 
+                                    <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col-reverse sm:flex-row flex-wrap justify-evenly content-center items-center">
+                                        <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start" >
+                                            <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
+                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.productName}</h1>
+                                            <p className="w-[80%] flex my-8">{featuredProduct.productDescription}</p>
+                    
+                                            <button onClick={() => handleDescription(1)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
+                                                <span className="inline-flex items-center"> 
+                                                    SHOP NOW
+                                                    <BsArrowRight className="ml-2" />
+                                                </span>
+                                            </button>
+                                        </div>
                 
-                <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col sm:flex-row flex-wrap justify-evenly content-center items-center">
-                    <div className="flex 4xs:w-[100%] sm:w-[40%] 4xs:h-[70%] sm:h-auto transform transition duration-500 hover:scale-90 bg-gray-100">
-                        <img src={Image1} alt="product" />
-                    </div>
+                                        <div className="flex justify-end 4xs:w-[100%] sm:w-[40%] 4xs:h-[70%] sm:h-auto transform transition duration-500 hover:scale-90 bg-gray-100 ">
+                                            <img src={featuredProduct.image} alt="product" />
+                                        </div>
+                                    </div>
+                                : 
+                                    <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col sm:flex-row flex-wrap justify-evenly content-center items-center">
+                                        <div className="flex 4xs:w-[100%] sm:w-[40%] 4xs:h-[70%] sm:h-auto transform transition duration-500 hover:scale-90 bg-gray-100">
+                                            <img src={featuredProduct.image} alt="product" />
+                                        </div>
+                
+                                        <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start ">
+                                            <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
+                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.productName}</h1>
+                                            <p className="w-[80%] flex my-8">{featuredProduct.productDescription} </p>
+                    
+                                            <button onClick={() => handleDescription(1)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
+                                                <span className="inline-flex items-center"> 
+                                                    SHOP NOW
+                                                    <BsArrowRight className="ml-2" />
+                                                </span>
+                    
+                                            </button>
+                                        </div>
+                                    </div>
+                            }
+                        </div>
 
-                    <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start ">
-                        <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
-                        <h1 className="text-5xl font-bold hover:text-red-900">Nancy Chair</h1>
-                        <p className="w-[80%] flex my-8">
-                            When an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
-                        </p>
-
-                        <button className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
-                            <span className="inline-flex items-center"> 
-                                SHOP NOW
-                                <BsArrowRight className="ml-2" />
-                            </span>
-
-                        </button>
-                    </div>
-                </div>
-
-                <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col-reverse sm:flex-row flex-wrap justify-evenly content-center items-center">
-                    <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start" >
-                        <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
-                        <h1 className="text-5xl font-bold hover:text-red-900">Nancy Chair</h1>
-                        <p className="w-[80%] flex my-8">
-                            When an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
-                        </p>
-
-                        <button className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
-                            <span className="inline-flex items-center"> 
-                                SHOP NOW
-                                <BsArrowRight className="ml-2" />
-                            </span>
-
-                        </button>
-                    </div>
-
-                    <div className="flex justify-end 4xs:w-[100%] sm:w-[40%] 4xs:h-[70%] sm:h-auto transform transition duration-500 hover:scale-90 bg-gray-100 ">
-                        <img src={Image1} alt="product" />
-                    </div>
-                </div>
-
-                <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col sm:flex-row flex-wrap justify-evenly content-center items-center">
-
-                    <div className="flex 4xs:w-[100%] sm:w-[40%] 4xs:h-[70%] sm:h-auto transform transition duration-500 hover:scale-90 bg-gray-100 ">
-                        <img src={Image1} alt="product" />
-                    </div>
-
-                    <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start ">
-                        <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
-                        <h1 className="text-5xl font-bold hover:text-red-900">Nancy Chair</h1>
-                        <p className="w-[80%] flex my-8">
-                            When an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
-                        </p>
-
-                        <button className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
-                            <span className="inline-flex items-center"> 
-                                SHOP NOW
-                                <BsArrowRight className="ml-2" />
-                            </span>
-                        </button>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
         </div>
     )
