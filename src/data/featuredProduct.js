e4ade542daf510242d0b63db3f5e714798b86dcc
Debbie <asamoahjaeldeborah@gmail.com>
2022-12-07 12:22:54 +0000

    Added the featured Products data

---
+++ src/data/featuredProduct.js
@@ -0,0 +1,27 @@
+import Chair from "../assets/chairs/CH2.png"
+import Dining from "../assets/dining/DS2.jpg"
+import Bed from "../assets/beds/BE2.jpg"
+
+
+const FeaturedProduct = [
+    {
+        image: Bed,
+        productName: "Penty Bed",
+        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
+        shop: "beds"
+    },
+    {
+        image: Chair,
+        productName: "Ammorexa Chair",
+        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
+        shop: "chairs"
+    },
+    {
+        image: Dining,
+        productName: "Xceles",
+        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
+        shop: "dining"
+    }
+]
+
+export default FeaturedProduct;
