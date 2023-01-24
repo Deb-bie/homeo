7e78f18100cb73da920d6ba8d1f43f18f24b665b
Debbie <asamoahjaeldeborah@gmail.com>
2023-01-24 17:16:11 +0000

    Changes to blog data

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,24 +1,27 @@
 
-import Image from "../assets/images/1.jpg"
+import Image from "../assets/images/1.jpg";
+import Image2 from "../assets/images/2.jpg"
+import Image3 from "../assets/images/3.jpg"
+
 
 const BlogData = [
     {
         id: 1,
         image: Image,
-        title: "A new way to beautify your home in 2022",
-        date: "November 18, 2022"
+        title: "A new way to beautify your home in 2023",
+        date: "January 8, 2023"
     },
     {
         id: 2,
-        image: Image,
-        title: "A new way to beautify your home in 2022",
-        date: "November 18, 2022"
+        image: Image2,
+        title: "2022 trends in retrospect",
+        date: "January 13, 2023"
     },
     {
         id: 3,
-        image: Image,
-        title: "A new way to beautify your home in 2022",
-        date: "November 18, 2022"
+        image: Image3,
+        title: "New trends in 2023",
+        date: "January 28, 2023"
     }
 ]
 
