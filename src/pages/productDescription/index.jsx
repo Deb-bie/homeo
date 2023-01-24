ba2b044b1c6b31c9bf8646a80844ab85e14825f9
Debbie <asamoahjaeldeborah@gmail.com>
2023-01-24 17:15:05 +0000

    Changed product description data from static to dynamic

--- src/pages/productDescription/index.jsx
+++ src/pages/productDescription/index.jsx
@@ -1,12 +1,14 @@
 import React from "react"
-import {Link} from "react-router-dom"
-
+import {Link, useLocation} from "react-router-dom"
 import {IoHeartOutline} from "react-icons/io5";
 import {GiArrowScope} from "react-icons/gi"
 
 import Image1 from "../../assets/images/1.jpg"
 
-const ProductDescriptionPage = () => {
+const ProductDescriptionPage = ({product}) => {
+    const location = useLocation();
+
+
     return (
         <div className="w-[100%] h-full flex overflow-hidden ">
             <div className="w-[100%] h-full flex flex-col flex-nowrap content-center items-start ">
@@ -15,11 +17,11 @@ const ProductDescriptionPage = () => {
                     <h1 className="w-[100%] text-xl sm:text-4xl font-semibold flex justify-start items-start ">Product Description</h1>
                     <span className="w-[100%] text-base font-[300] flex flex-row justify-end ">
                         <span className="4xs:w-[70%] sm:w-[30%] flex justify-around sm:pr-0 pr-8 ">
-                            <Link to="/product">PRODUCT</Link>
+                            <Link to="/products">PRODUCTS</Link>
                             &nbsp;
                             <h2>/</h2>
                             &nbsp;
-                            <h2 className="text-gray-500">PRODUCT NAME</h2>
+                            <h2 className="text-gray-500">{location.state.product.name}</h2>
                         </span>
                     </span>
                 </div>
@@ -27,14 +29,13 @@ const ProductDescriptionPage = () => {
                 <div className="mt-12 w-[100%] h-full gap-4 p-6 4xs:px-4 sm:px-12 lg:px-32 flex 4xs:flex-col sm:flex-row justify-between content-center items-start mb-12">
 
                     <div className="4xs:w-[100%] sm:w-[40%] h-[50%] flex flex-row bg-gray-100 " >
-                        <img src={Image1} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
+                        <img src={location.state.product.image} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
                     </div>
 
                     <div className="4xs:w-[100%] sm:w-[40%] 4xs:p-[1px] sm:pl-6  flex flex-col justify-start content-start items-start 4xs:mb-4 sm:mb-8 " >
-                        <h1 className="flex flex-row text-4xl font-semibold mb-4">Namcy Chair</h1>
+                        <h1 className="flex flex-row text-4xl font-semibold mb-4">{location.state.product.name}</h1>
                         <h1 className="flex flex-row text-3xl font-semibold text-gray-600 mb-4">$100.00</h1>
-                        <p className="text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore fugiat vel molestias aspernatur, 
-                            Fugit hic reprehenderit quas? 
+                        <p className="text-base mb-4">{location.state.product.description}
                         </p>
 
                         <div className="4xs:w-[100%] sm:w-[100%] flex 4xs:flex-col gap-6 sm:flex-row justify-between content-center items-center mb-8  ">
@@ -53,7 +54,7 @@ const ProductDescriptionPage = () => {
                             </div>
                         </div>
 
-                        <h1 className="flex text-xl">Category: Beds</h1>
+                        <h1 className="flex text-xl">Category: {location.state.product.category}</h1>
                     </div>
 
                 </div>
@@ -64,13 +65,11 @@ const ProductDescriptionPage = () => {
                 <div className="w-[100%] h-full gap-6 p-6 4xs:px-4 sm:px-12 lg:px-32 flex 4xs:flex-col sm:flex-row justify-between content-center items-center mb-6 ">
                     <div className="4xs:w-[100%] sm:w-[40%] flex flex-col">
                         <h1 className="flex text-3xl mb-4">Description</h1>
-                        <p className="flex text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, autem quisquam nobis in error nulla ea totam rem aut adipisci 
-                            illo atque magni, doloremque itaque ex iure aliquid illum tempora! 
-                        </p>
+                        <p className="flex text-lg">{location.state.product.description}</p>
                     </div>
 
                     <div className="4xs:w-[100%] sm:w-[40%] h-[50%] flex flex-row  bg-gray-100  " >
-                        <img src={Image1} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
+                        <img src={location.state.product.image} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
                     </div>
                 </div>
 
@@ -83,13 +82,13 @@ const ProductDescriptionPage = () => {
                         <ul>
                             <li className="flex flex-row items-center content-center ">
                                 <GiArrowScope className="mr-2 text-lg "/>
-                                Lorem  consequuntur fuga maxime dignissimos eius eos  
+                                {location.state.product.additionalInformation}
                             </li>
                         </ul>
                     </div>
 
                     <div className="4xs:w-[100%] sm:w-[40%] h-[50%] flex flex-row bg-gray-100  " >
-                        <img src={Image1} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
+                        <img src={location.state.product.image} alt="product" className="flex w-[100%] h-[100%] object-contain   " />
                     </div>
                 </div>
 
