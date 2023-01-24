950b5761704694acf4d64c81fa2f4844cef5cb71
Debbie <asamoahjaeldeborah@gmail.com>
2023-01-24 17:09:30 +0000

    Fixed the link between featured products and the product description page

--- src/components/featuredProducts/index.jsx
+++ src/components/featuredProducts/index.jsx
@@ -7,8 +7,27 @@ import FeaturedProduct from "../../data/featuredProduct.js"
 const FeaturedProducts = () => {
     const navigate = useNavigate()
 
-    const handleDescription = (id) => {
-        navigate(`/products/${id}`)
+    const handleDescription = (product) => {
+            console.log(product)
+            navigate(
+                `/products/${product._id}`,
+                {
+                    state: {
+                        // _id: product._id,
+                        // name: product.name,
+                        // description: product.description,
+                        // category: product.category,
+                        // stock: product.stock,
+                        // price: product.price,
+                        // availability: product.availability,
+                        // additionalInformation: product.additionalInformation,
+                        // image: product.image,
+                        // featured: product.featured,
+                        product: product
+                    }
+                }
+            )
+        
     }
 
 
@@ -24,10 +43,10 @@ const FeaturedProducts = () => {
                                     <div className="px-6 mb-24 4xs:gap-8 sm:gap-0 w-[100%] h-full flex flex-col-reverse sm:flex-row flex-wrap justify-evenly content-center items-center">
                                         <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start" >
                                             <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
-                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.productName}</h1>
-                                            <p className="w-[80%] flex my-8">{featuredProduct.productDescription}</p>
+                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.name}</h1>
+                                            <p className="w-[80%] flex my-8">{featuredProduct.description}</p>
                     
-                                            <button onClick={() => handleDescription(1)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
+                                            <button onClick={() => handleDescription(featuredProduct)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
                                                 <span className="inline-flex items-center"> 
                                                     SHOP NOW
                                                     <BsArrowRight className="ml-2" />
@@ -47,10 +66,10 @@ const FeaturedProducts = () => {
                 
                                         <div className="4xs:w-full sm:w-[40%] h-full flex flex-col flex-wrap 4xs:justify-stretch sm:justify-center content-center items-start ">
                                             <h6 className="text-gray-300 font-semibold text-base ">FEATURED PRODUCT</h6>
-                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.productName}</h1>
-                                            <p className="w-[80%] flex my-8">{featuredProduct.productDescription} </p>
+                                            <h1 className="text-5xl font-bold hover:text-red-900">{featuredProduct.name}</h1>
+                                            <p className="w-[80%] flex my-8">{featuredProduct.description} </p>
                     
-                                            <button onClick={() => handleDescription(1)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
+                                            <button onClick={() => handleDescription(featuredProduct)} className="border-2 border-black py-2 px-4 transform transition duration-500 hover:bg-black hover:text-white ">
                                                 <span className="inline-flex items-center"> 
                                                     SHOP NOW
                                                     <BsArrowRight className="ml-2" />
--- src/data/featuredProduct.js
+++ src/data/featuredProduct.js
@@ -5,23 +5,53 @@ import Bed from "../assets/beds/BE2.jpg"
 
 const FeaturedProduct = [
     {
-        image: Bed,
-        productName: "Penty Bed",
-        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
-        shop: "beds"
-    },
-    {
-        image: Chair,
-        productName: "Ammorexa Chair",
-        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
-        shop: "chairs"
-    },
-    {
-        image: Dining,
-        productName: "Xceles",
-        productDescription: "When an unknown printer took a galley of type and scrambled it to make a type specimen book. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.",
-        shop: "dining"
-    }
+        _id: "639dae9853a950b14c63c632",
+        name: "Ammorexa Chair",
+        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        category: "chairs",
+        stock: 9,
+        price: 60,
+        availability: "in-stock",
+        additionalInformation: "Lorem ipsum is placeholder \
+        text commonly used in the ",
+        image: "http://res.cloudinary.com/dbedwytfr/image/upload/v1671278743/xw3lqbmw10ernoezz6up.png",
+        featured: false,
+        createdAt: "2022-12-17T11:57:12.407Z",
+        updatedAt: "2022-12-17T12:05:44.907Z",
+        __v: 0
+        },
+        {
+        _id: "639dafa253a950b14c63c6e3",
+        name: "Bret",
+        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        category: "beds",
+        stock: 12,
+        price: 218,
+        availability: "in-stock",
+        additionalInformation: "Lorem ipsum is placeholder \
+        text commonly used in the graphic, ",
+        image: "http://res.cloudinary.com/dbedwytfr/image/upload/v1671278497/arkkpwyzmxelaybrk4pi.jpg",
+        featured: false,
+        createdAt: "2022-12-17T12:01:38.999Z",
+        updatedAt: "2022-12-17T12:01:38.999Z",
+        __v: 0
+        },
+        {
+        _id: "639daff553a950b14c63c736",
+        name: "Teil",
+        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+        category: "couches",
+        stock: 5,
+        price: 150,
+        availability: "in-stock",
+        additionalInformation: "Lorem ipsum is placeholder \
+        text commonly used in the graphic",
+        image: "http://res.cloudinary.com/dbedwytfr/image/upload/v1671278580/jgklxeztjj42eibezmrc.png",
+        featured: false,
+        createdAt: "2022-12-17T12:03:01.459Z",
+        updatedAt: "2022-12-17T12:03:01.459Z",
+        __v: 0
+        },
 ]
 
 export default FeaturedProduct;
